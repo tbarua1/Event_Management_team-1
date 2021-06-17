fb35cf0aa597a9dd0a0015d6b39ea228e80ced06
Anandkumarreddypalakondu <85949281+Anandkumarreddypalakondu@users.noreply.github.com>
2021-06-17 13:31:31 +0530

    Add files via upload
    this is anand demo file

---
+++ A.java
@@ -0,0 +1,24 @@
+package demo;
+
+public class A {
+	public static  int x=20;
+	public static void b() {
+		int count=15;
+		int a,b,c,d;
+		a=count++;
+		b=count;
+		c=++count;
+		d=count;
+		System.out.println(a+","+b+","+c+","+d);
+	}
+	public static void main(String[] args) {
+	   int y=0;
+		y=x;
+		++y;
+		System.out.println("x is :"+x);
+		System.out.println("y is :"+y);
+		b();
+	}
+	
+
+}

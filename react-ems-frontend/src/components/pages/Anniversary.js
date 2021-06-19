247e931f8256e6a1a2f5d464af6f31bbb927dba3
Vivek <cognixia.team1@gmail.com>
2021-06-19 17:07:58 +0530

    Added blueprint front-end of our site

---
+++ react-ems-frontend/src/components/pages/Anniversary.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Anniversary() {
+  return (
+    <>
+      <h1 className='anniversary'>ANNIVERSARY</h1>
+    </>
+  );
+}
---
+++ react-ems-frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { GlobalStyles } from './global-styles';
+import App from './App';
+
+ReactDOM.render(
+    <App />, document.getElementById('root'));
+    // <>
+    // <GlobalStyles />
+    // <App />
+    // </>

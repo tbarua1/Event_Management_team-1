46720f1bbe38aa368d9c7bd6a0685d8e5e1e14cd
Aravind-arjun <84772692+Aravind-arjun@users.noreply.github.com>
2021-06-22 21:45:51 +0530

    Add files via upload

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: 'Wedding',
+    path: '/wedding',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Anniversary',
+    path: '/anniversary',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Birthday Party',
+    path: '/birthday',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Social Get-together',
+    path: '/gettogether',
+    cName: 'dropdown-link'
+  }
+];
---
+++ src/components/pages/Wedding.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Wedding() {
+  return (
+    <>
+      <h1 className='wedding'>WEDDING</h1>
+    </>
+  );
+}

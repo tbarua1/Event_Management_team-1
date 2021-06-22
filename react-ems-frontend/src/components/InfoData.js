05fc857e35ad15b57f2eacee1715e45d393bbfc6
team-vivek <85878624+team-vivek@users.noreply.github.com>
2021-06-22 20:50:58 +0530

    Added contents of home page

---
+++ react-ems-frontend/src/components/InfoData.js
@@ -0,0 +1,21 @@
+import image1 from '../images/image3.jpg'
+import image2 from '../images/image5.jpg'
+
+export const InfoData = {
+    heading: 'Explore our wedding Venues',
+    paragraph1: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.',
+    paragraph2: 'the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.',
+    buttonLabel: 'Read more',
+    image: image1,
+    reverse: false,
+    delay: 100
+};
+export const InfoData2 = {
+    heading: 'Explore our Concert Venues',
+    paragraph1: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.',
+    paragraph2: 'the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.',
+    buttonLabel: 'Read more',
+    image: image2,
+    reverse: true,
+    delay: 300
+};
---
+++ react-ems-frontend/src/components/SliderData.js
@@ -0,0 +1,39 @@
+import Image1 from '../images/image1.jpg'
+import Image2 from '../images/image2.jpg'
+import Image3 from '../images/image3.jpg'
+import Image4 from '../images/image4.jpg'
+
+export const SliderData =[
+    {
+        title: 'Luxury Farm-house in Mumbai, India',
+        price: '$428/hr',
+        path: '/contents',
+        label: 'Book Now',
+        image: Image1,
+        alt: 'venue'
+    },
+    {
+        title: 'Concert Halls in Bali, Indonesia',
+        price: '$1000/hr',
+        path: '/contents',
+        label: 'Book Now',
+        image: Image2,
+        alt: 'venue'
+    },
+    {
+        title: 'Luxury Wedding Venue in Texas, USA',
+        price: '$600/hr',
+        path: '/contents',
+        label: 'Book Now',
+        image: Image3,
+        alt: 'venue'
+    },
+    {
+        title: 'Party house in Italy',
+        price: '$428/hr',
+        path: '/contents',
+        label: 'Book Now',
+        image: Image4,
+        alt: 'venue'
+    },
+];

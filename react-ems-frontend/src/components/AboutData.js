e58158b68b6f32f3d323343a1bff64d93439590e
team-vivek <85878624+team-vivek@users.noreply.github.com>
2021-06-22 20:47:05 +0530

    Adding content to about us page

---
+++ react-ems-frontend/src/components/AboutData.js
@@ -0,0 +1,49 @@
+import image1 from '../images/p1.jpg'
+import image2 from '../images/p3.jpg'
+import image3 from '../images/p2.jpg'
+import image4 from '../images/p5.jpg'
+import image5 from '../images/p4.jpg'
+
+export const AboutData = {
+    heading: 'Vivek',
+    paragraph1: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.',
+    paragraph2: 'the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.',
+    buttonLabel: 'Read more',
+    image: image1,
+    reverse: false,
+    delay: 100
+};
+export const AboutData2 = {
+    heading: 'Jubika',
+    paragraph1: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.',
+    paragraph2: 'the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.',
+    buttonLabel: 'Read more',
+    image: image2,
+    reverse: true,
+    delay: 300
+};
+export const AboutData3 = {
+    heading: 'Anand',
+    paragraph1: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.',
+    paragraph2: 'the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.',
+    buttonLabel: 'Read more',
+    image: image3,
+    reverse: true,
+    delay: 300
+};export const AboutData4 = {
+    heading: 'Aravind',
+    paragraph1: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.',
+    paragraph2: 'the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.',
+    buttonLabel: 'Read more',
+    image: image4,
+    reverse: true,
+    delay: 300
+};export const AboutData5 = {
+    heading: 'Pooja',
+    paragraph1: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.',
+    paragraph2: 'the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure.',
+    buttonLabel: 'Read more',
+    image: image5,
+    reverse: true,
+    delay: 300
+};

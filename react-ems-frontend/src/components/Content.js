8429f256d7ade64c4b2f6771bd04cdbcde407bc6
team-vivek <85878624+team-vivek@users.noreply.github.com>
2021-06-22 21:48:27 +0530

    enabling auto slide feat. in image

--- react-ems-frontend/src/components/Content.js
+++ react-ems-frontend/src/components/Content.js
@@ -128,18 +128,18 @@ const Content = ({ slides }) => {
     const length = slides.length
     const timeout = useRef(null)
 
-    // useEffect(() =>{
-    //     const nextSlide = () => {
-    //         setCurrent(current =>(current === length-1 ? 0: current + 1))
-    //     }
-    //     timeout.current =setTimeout(nextSlide, 3000)
+    useEffect(() =>{
+        const nextSlide = () => {
+            setCurrent(current =>(current === length-1 ? 0: current + 1))
+        }
+        timeout.current =setTimeout(nextSlide, 3000)
 
-    //     return function (){
-    //         if(timeout.current){
-    //             clearTimeout(timeout.current)
-    //         }
-    //     }
-    // }, [current, length])
+        return function (){
+            if(timeout.current){
+                clearTimeout(timeout.current)
+            }
+        }
+    }, [current, length])
 
     const nextSlide = () => {
         setCurrent(current === length - 1 ? 0 : current + 1);

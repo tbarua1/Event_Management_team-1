a8a2fdcf3c32a1de955cd17ef841a311c38bc675
team-vivek <85878624+team-vivek@users.noreply.github.com>
2021-06-22 22:29:20 +0530

    updated app.js

--- react-ems-frontend/src/App.js
+++ react-ems-frontend/src/App.js
@@ -10,6 +10,8 @@ import SignUp from './components/pages/SignUp';
 import Wedding from './components/pages/Wedding';
 import Anniversary from './components/pages/Anniversary';
 import { FooterContainer } from './containers/footer';
+import AboutUs from './components/pages/AboutUs';
+import Registration from './components/pages/Registration';
 
 function App() {
   return (
@@ -19,10 +21,12 @@ function App() {
         <Route path='/' exact component={Home} />
         <Route path='/events' component={Events} />
         <Route path='/blogs' component={Blog} />
+        <Route path='/contents' component={AboutUs} />
         <Route path='/contact-us' component={ContactUs} />
         <Route path='/sign-up' component={SignUp} />
         <Route path='/wedding' component={Wedding} />
         <Route path='/anniversary' component={Anniversary} />
+        <Route path='/reg' component={Registration} />
       </Switch>
       <FooterContainer />
     </Router>

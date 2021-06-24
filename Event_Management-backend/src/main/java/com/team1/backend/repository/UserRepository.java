afb5f727736eb888fefc530f3678ac2a409ff6b5
Anandkumarreddypalakondu <85949281+Anandkumarreddypalakondu@users.noreply.github.com>
2021-06-24 12:13:48 +0530

    Update UserRepository.java

--- Event_Management-backend/src/main/java/com/team1/backend/repository/UserRepository.java
+++ Event_Management-backend/src/main/java/com/team1/backend/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.team1.backend.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import module.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }

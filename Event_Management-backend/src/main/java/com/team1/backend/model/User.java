8f223085732a01cda1b52b89dbe17b61b23c9661
team-vivek <85878624+team-vivek@users.noreply.github.com>
2021-06-24 20:31:09 +0530

    added user model

--- Event_Management-backend/src/main/java/com/team1/backend/model/User.java
+++ Event_Management-backend/src/main/java/com/team1/backend/model/User.java
@@ -1,5 +1,80 @@
 package com.team1.backend.model;
 
-public class User {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long uid;
+	private String uName;
+	private String uEmail;
+	private String uPass;
+	
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user", fetch = FetchType.LAZY)
+	private List<Event> events = new ArrayList<>();
+	
+	@JsonManagedReference
+	public List<Event> getEvents() {
+		return events;
+	}
+	public void setEvents(List<Event> events) {
+		this.events = events;
+	}
+	
+	
+	public Long getUid() {
+		return uid;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setUid(Long uid) {
+		this.uid = uid;
+	}
+	public String getuName() {
+		return uName;
+	}
+	public void setuName(String uName) {
+		this.uName = uName;
+	}
+	public String getuEmail() {
+		return uEmail;
+	}
+	public void setuEmail(String uEmail) {
+		this.uEmail = uEmail;
+	}
+	public String getuPass() {
+		return uPass;
+	}
+	public void setuPass(String uPass) {
+		this.uPass = uPass;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [uid=" + uid + ", uName=" + uName + ", uEmail=" + uEmail + ", uPass=" + uPass + ", events="
+				+ events + "]";
+	}
+	
+	
 }
+

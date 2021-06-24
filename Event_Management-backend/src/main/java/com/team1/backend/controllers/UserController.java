b22946724777c8091ea0e2c1997078c7e7007bd5
poojapbd123 <51699687+poojapbd123@users.noreply.github.com>
2021-06-23 23:42:22 -0700

    Add files via upload

--- Event_Management-backend/src/main/java/com/team1/backend/controllers/UserController.java
+++ Event_Management-backend/src/main/java/com/team1/backend/controllers/UserController.java
@@ -1,5 +1,52 @@
-package com.team1.backend.controllers;
+package com.example.demo;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController 
+{
+@Autowired
+	
+	private UserRepository repo; 
+
+	@RequestMapping (value = "/user", method =RequestMethod.GET)
+	public List<User> userList()
+	{
+		return repo.findAll();
+	}
+	
+	@PostMapping("/user")
+	public User userPost(@RequestBody User User)
+	{
+		return repo.save(User);
+		
+	}
+	
+	
+   @DeleteMapping("/user")
+   public String UserDelete(@RequestParam Long id)
+	{
+		 repo.deleteById(id);
+		return "record no."+id+" has been deleted ";
+		
+		
+	}
+   @PutMapping("/user")
+   public User userPut(@RequestBody User User)
+	{
+		return repo.save(User);
+		
+	}
 }
+
+

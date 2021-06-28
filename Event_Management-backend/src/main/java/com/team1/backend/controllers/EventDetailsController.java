3e9d45e0cce858984c5a7aaf536efa6d65034f05
team-vivek <85878624+team-vivek@users.noreply.github.com>
2021-06-28 09:43:55 +0530

    updating event controller

--- Event_Management-backend/src/main/java/com/team1/backend/controllers/EventDetailsController.java
+++ Event_Management-backend/src/main/java/com/team1/backend/controllers/EventDetailsController.java
@@ -1,47 +1,42 @@
-package com.example.demo;
+package com.team1.backend.controllers;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.team1.backend.model.Event;
+import com.team1.backend.repository.EventRepository;
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-public class EventDetailsController 
-{
+public class EventDetailController {
 	@Autowired
-	
-	private EventsRepository repo;
-	@RequestMapping (value = "/event", method =RequestMethod.GET)
-	public List<Events> eventList()
-	{
+	private EventRepository repo;
+	@RequestMapping(value = "/get_event", method = RequestMethod.GET)
+	public List<Event> eventList() {
 		return repo.findAll();
 	}
-	
 	@PostMapping("/event")
-	public Events eventPost(@RequestBody Events event)
-	{
+	public Event eventPost(@RequestBody Event event) {
 		return repo.save(event);
-		
 	}
-	
-   @DeleteMapping("/event")
-   public String eventDelete(@RequestBody Long id)
-	{
-		 repo.deleteById(id);
-		return "record no."+id+" has been deleted ";
-		
-		
+	@DeleteMapping("/event")
+	public String eventDelete(@RequestParam Long id) {	
+		repo.deleteById(id);
+		return "Record No. "+id+" has been Deleted";
 	}
-   @PutMapping("/event")
-   public Events eventPut(@RequestBody Events event)
-	{
+	@PutMapping("/event")
+	public Event eventPut(@RequestBody Event event) {
 		return repo.save(event);
-		
 	}
 }
+

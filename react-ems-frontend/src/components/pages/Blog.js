023eaa37be2bd9c1c4ea794870b473dadb9336f2
Aravind-arjun <84772692+Aravind-arjun@users.noreply.github.com>
2021-06-28 21:31:28 +0530

    Update Blog.js

--- react-ems-frontend/src/components/pages/Blog.js
+++ react-ems-frontend/src/components/pages/Blog.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import '../../App.css';
+import './Blog.css';
 
-export default function Blog() {
-  return <h1 className='blogs'>BLOGS</h1>;
+
+const BlogUI = props => {
+    return (
+        <div className="Blog text-center shadow">
+            <div className="overflow">
+                <img src={props.imgsrc} alt='image' className="card-img-top" />
+
+                <div className="Blog-body text-dark">
+                    <h4 className="card-title">{props.title}</h4>
+                    <p className="card-text text-secondary">{props.msg}</p>
+                    <a href="#" className="btn btn-outline-success">know More</a>
+                </div>
+            </div>
+        </div>
+    );
 }
+export default BlogUI

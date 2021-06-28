fc62deae68dd7a0986a2b78c21512c8576f6c547
team-vivek <85878624+team-vivek@users.noreply.github.com>
2021-06-28 15:49:19 +0530

    connected signup form with backend

--- react-ems-frontend/src/components/pages/SignUp.js
+++ react-ems-frontend/src/components/pages/SignUp.js
@@ -1,44 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../App.css';
 import '../../SignUpStyle.css'
+import axios from 'axios';
+import base_url from '../../services/EventService';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function SignUp() {
-  return(
+
+  const [user, setUser] = useState({});
+
+  const handleForm = (e) => {
+    console.log(user);
+    postDataToServer(user);
+    e.preventDefault();
+  };
+
+  const postDataToServer = (data) => {
+    axios.post(`${base_url}/user`, data).then(
+      (response) => {
+        console.log(response);
+        console.log("success");
+
+      },
+      (error) => {
+        console.log(error);
+        console.log("error");
+      }
+    )
+  };
+
+  const btnHandle = () => {
+    toast('🦄 Registered successfully! You can login now', {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
+  return (
     <div className="rsign">
-     <form action="">
-      <div id="login-box">
-        <div class="left-box">
-          <h1 class="heading"> Register</h1>
-         <label htmlFor="username">Username </label>
-        <input className="input" type="text" name="username" placeholder="Name" ></input>
-        <label htmlFor="email">Email </label>
-        <input className="input" type="text" name="email" placeholder="Email" ></input>
-        <label htmlFor="Password">Password </label>
-        <input className="input" type="text" name="pass" placeholder="Password" ></input>
-        <label htmlFor="Password">Password </label>
-        <input className="input" type="text" name="pass2" placeholder="Confirm Password" ></input>
-        <input type="submit" name="signup button" value="Submit"></input>
+      <ToastContainer position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover />
+      <form onSubmit={handleForm}>
+        <div id="login-box">
+          <div class="left-box">
+            <h1 class="heading"> Register</h1>
+            <label htmlFor="username">Username </label>
+            <input className="input" type="text" name="username" placeholder="Name" onChange={(e) => {
+              setUser({ ...user, uName: e.target.value })
+            }}></input>
+            <label htmlFor="email">Email </label>
+            <input className="input" type="text" name="email" placeholder="Email" onChange={(e) => {
+              setUser({ ...user, uEmail: e.target.value })
+            }}></input>
+            <label htmlFor="Password">Password </label>
+            <input className="input" type="text" name="pass" placeholder="Password" onChange={(e) => {
+              setUser({ ...user, uPass: e.target.value })
+            }}></input>
+            <label htmlFor="Password">Password </label>
+            <input className="input" type="text" name="pass2" placeholder="Confirm Password" ></input>
+            <input type="submit" name="signup button" value="Submit" onClick={btnHandle} ></input>
           </div>
-        <div class="right-box">
-        <span class="signinwith"></span>
-<button class="social facebook">Login with facebook</button>
-<button class="social twitter">Login with twitter</button>
-<button class="social google">Login with google</button>
-</div>
-<div class="or">
-  <p class="o">OR</p>
-  </div>
-</div>
-</form>
-</div>
-    
-    
-
-);  }
+          <div class="right-box">
+            <span class="signinwith"></span>
+            <button class="social facebook">Login with facebook</button>
+            <button class="social twitter">Login with twitter</button>
+            <button class="social google">Login with google</button>
+          </div>
+          <div class="or">
+            <p class="o">OR</p>
+          </div>
+        </div>
+      </form>
+    </div>
 
 
 
-
-
-
-
+  );
+}

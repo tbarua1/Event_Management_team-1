201b2a52e5aeda0402a1c3f622a447fbf550dac7
team-vivek <85878624+team-vivek@users.noreply.github.com>
2021-06-28 09:46:26 +0530

    updating user repository

--- Event_Management-backend/src/main/java/com/team1/backend/repository/EventRepository.java
+++ Event_Management-backend/src/main/java/com/team1/backend/repository/EventRepository.java
@@ -3,9 +3,8 @@ package com.team1.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.team1.backend.model.Event;
-
+import com.team1.backend.model.User;
 @Repository
-public interface EventRepository extends JpaRepository<Event, Long>{
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }

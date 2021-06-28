29b602c7f5322658191cd7864c0b6fbd051590a7
team-vivek <85878624+team-vivek@users.noreply.github.com>
2021-06-28 09:42:45 +0530

    updating user controller

--- Event_Management-backend/src/main/java/com/team1/backend/controllers/UserController.java
+++ Event_Management-backend/src/main/java/com/team1/backend/controllers/UserController.java
@@ -1,8 +1,10 @@
-package com.example.demo;
+package com.team1.backend.controllers;
+
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,40 +14,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class UserController 
-{
-@Autowired
-	
-	private UserRepository repo; 
+import com.team1.backend.model.User;
+import com.team1.backend.repository.UserRepository;
 
-	@RequestMapping (value = "/user", method =RequestMethod.GET)
-	public List<User> userList()
-	{
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+public class UserController {
+	@Autowired
+	private UserRepository repo;
+	
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public List<User> userList() {
 		return repo.findAll();
 	}
-	
 	@PostMapping("/user")
-	public User userPost(@RequestBody User User)
-	{
+	public User userPost(@RequestBody User User) {
 		return repo.save(User);
-		
 	}
-	
-	
-   @DeleteMapping("/user")
-   public String UserDelete(@RequestParam Long id)
-	{
-		 repo.deleteById(id);
-		return "record no."+id+" has been deleted ";
-		
-		
+	@DeleteMapping("/user")
+	public String userDelete(@RequestParam Long id) {	
+		repo.deleteById(id);
+		return "Record No. "+id+" has been Deleted";
 	}
-   @PutMapping("/user")
-   public User userPut(@RequestBody User User)
-	{
+	@PutMapping("/user")
+	public User userPut(@RequestBody User User) {
 		return repo.save(User);
-		
 	}
 }
 

2d28b5c9e3c60a96270fa5984c2afd6f872ad748
team-vivek <85878624+team-vivek@users.noreply.github.com>
2021-06-28 12:38:45 +0530

    updated app.js

--- react-ems-frontend/src/App.js
+++ react-ems-frontend/src/App.js
@@ -7,11 +7,11 @@ import Events from './components/pages/Events';
 import Blog from './components/pages/Blog';
 import ContactUs from './components/pages/ContactUs';
 import SignUp from './components/pages/SignUp';
-import Wedding from './components/pages/Wedding';
-import Anniversary from './components/pages/Anniversary';
 import { FooterContainer } from './containers/footer';
 import AboutUs from './components/pages/AboutUs';
 import Registration from './components/pages/Registration';
+//import EventData from './components/EventData';
+import ListOfEvents from './components/ListOfEvents';
 
 function App() {
   return (
@@ -20,13 +20,12 @@ function App() {
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/events' component={Events} />
+        <Route path='/reg' component={Registration} />
         <Route path='/blogs' component={Blog} />
         <Route path='/contents' component={AboutUs} />
         <Route path='/contact-us' component={ContactUs} />
-        <Route path='/sign-up' component={SignUp} />
-        <Route path='/wedding' component={Wedding} />
-        <Route path='/anniversary' component={Anniversary} />
-        <Route path='/reg' component={Registration} />
+        <Route path='/sign-up' component={SignUp} /> 
+        <Route path='/admin' component={ListOfEvents} />        
       </Switch>
       <FooterContainer />
     </Router>
